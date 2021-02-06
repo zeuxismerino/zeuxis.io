f72e48c164c5aac6b2e86568cc4b2d464b7b61fc
zeuxismerino <merinozeuxis@gmail.com>
2021-02-06 13:26:55 -0500

    create and edit file index.html

---
+++ app.js
@@ -0,0 +1,154 @@
+// 1
+const num1 = 20,
+      num2 = 30;
+
+ let suma = (num1 + num2);
+
+console.log(suma);
+
+// 2
+const nota1 = 14,
+      nota2 = 12,
+      nota3 = 20,
+      nota4 = 8;
+
+let promedio = (nota1 + nota2 + nota3 + nota4) / 4;
+
+ console.log(promedio);
+
+// 3
+const base1= 20,
+      altura1 = 12;
+
+let area1 = (base1 * altura1);
+
+console.log(area1);
+
+// 4
+const base2 = 20,
+      altura2 = 12;
+    
+let area2 = (base2 * altura2) / 2;
+
+console.log(area2);
+
+// 5
+const π = 3.141529,
+      radio = 20;
+
+let area3 = π * Math.pow(radio, 2);
+
+console.log(area3);
+
+// 6
+
+
+// 7
+let metros = 40,
+    pulgada = metros * 0.0254; 
+
+pedido = `las pulgadas pedidas son ${pulgada}`;
+
+console.log(pedido);
+
+// 8
+const dolar = 4,
+      soles = 3.64;
+    
+cambio = dolar * soles;      
+
+console.log(cambio)
+
+// 9
+const añoNacimiento = 1992,
+      añoActual = 2021;
+
+edad = añoActual - añoNacimiento;
+
+console.log(`tu edad es ${edad}`);
+
+// 10
+const nombre1 = "pedro",
+      nombre2 = "zeus",
+      nombre3 = "luis";
+let     edad1 = 27;
+let     edad2 = 14;
+let     edad3 = 25;
+
+if(edad1 < edad2 ){
+    console.log(` ${nombre1} tiene ${edad1} años`);
+}
+else if(edad1 < edad3){
+    console.log(`${nombre1} tiene ${edad1} años `);
+}
+else if(edad2 < edad3){
+    console.log(`${nombre2} tiene ${edad2} años`);
+}
+else if(edad3 < edad2){
+    console.log(`${nombre3} tiene ${edad3} años`);
+}
+else if(edad3 < edad1){
+    console.log(`${nombre3} tiene ${edad3} años `);
+}
+
+// 11
+let antiguedad =7;
+let    bono= 1000;
+
+if (antiguedad ==1){
+    console.log(`${antiguedad} años de antiguedad resive ${bono} bono`);
+}
+else if (antiguedad ==2){
+    console.log(`${antiguedad} años de antiguedad resive ${bono} bono`);
+}
+else if (antiguedad ==3){
+    console.log(`${antiguedad} años de antiguedad resive ${bono} bono`);
+}
+else if (antiguedad == 4){
+   console.log(`${antiguedad} años de antiguedad resive ${bono} bono`);
+}else if (antiguedad == 5){
+    console.log(`${antiguedad} años de antiguedad resive ${bono} bono`);
+}
+else if (antiguedad > 5) {
+    console.log(`${antiguedad} años de antiguedad resive ${bono} bono`);
+}
+
+// 12
+let salario = 1500;
+let numeros = [1, 6];
+salarioAnual1=salario + (salario * 0.10);
+salarioAnual2= salarioAnual1 + (salarioAnual1 * 0.10)
+salarioAnual3= salarioAnual2 + (salarioAnual2 * 0.10)
+salarioAnual4= salarioAnual3 + (salarioAnual3 * 0.10)
+salarioAnual5= salarioAnual4 + (salarioAnual4 * 0.10)
+salarioAnual6= salarioAnual5 + (salarioAnual5 * 0.10)
+console.log("eñ salario en 5 años es: ",salarioAnual5)
+
+// 13
+let n= 2;
+let calificacion =14;
+
+reprobados= 0
+aprobados = 0
+if (n > 0){
+    calificacion= 13;
+    calificacion < 6
+    n = n -1
+console.log(reprobados)
+}
+
+// 15
+let mayorDeEdad= 14;
+
+if (mayorDeEdad >= 18){
+    console.log(` puede sufragar`)
+}
+else {
+    console.log(`no puede sufragar`)
+}
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,8 @@
+¿Cuál fue la razón de crear Javascript?
+originó la necesidad de concebir un lenguaje de programación que se ejecutara en un mismo navegador, con el fin de optimizar el tiempo de navegación de los usuarios; por ejemplo, si un usuario ingresó incorrectamente su información en un formulario, no tendría que esperar mucho para detectar y corregir los errores.
+
+¿Quién fue el creador de Javascript?
+ Brendan Eich, empleado de Netscape,en abril de 1995.
+
+¿Por qué se llamó Javascript?   
+Antes del lanzamiento, Netscape decidió cambiar el nombre a JavaScript. La razón para cambiar el nombre fue puramente de marketing, porque Java era una palabra de moda en el mundo de la informática e Internet en ese momento.
